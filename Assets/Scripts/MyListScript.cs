1694fafedfdb705520edf936398e92bc444f2b1b
freeze0 <kras2001@bk.ru>
2023-09-28 15:48:50 +0300

    homework №3 Реализовать MyList<T>

--- Assets/Scripts/MyListScript.cs
+++ Assets/Scripts/MyListScript.cs
@@ -9,76 +9,66 @@ public class MyListScript : MonoBehaviour
 {
 
 
-    public class MyList
-    {
-        private int[] m_array = new int[10];
-        private int m_count = 0;
-        private int m_capacity = 10;
+    public class MyClass
+    { 
+        private int[] m_array = new int[10]; // была проблема с capacity если оставить пустым
 
-        public int Count
-        {
-            get
-            {
-                return m_count;
-            }
-            set 
-            {
-                if (value <= m_capacity)
-                {
-                    this.m_count = value;
-                }
-                else
-                {
-                    throw new Exception("Invalid count");
-                }
-            }
-        }
+        public int Count { get; private set; }
 
         public int Capacity
         {
             get
             {
-                return m_capacity;
+                return m_array.Length; // здесь была ошибка NullReferenceException
             }
             set
             {
-                this.m_capacity = value;
-                int [] _m_array = new int[m_capacity];
-                for (int i = 0; i < m_capacity; i++)
+                if (value > m_array.Length)
                 {
-                    _m_array[i] = m_array[i];
+                    System.Array.Resize(ref m_array, value);
                 }
-                m_array = _m_array;
             }
         }
 
-        public MyList()
+        public MyClass()
         {
 
         }
 
-        public MyList(int capacity)
+        public MyClass(int capacity)
         {
-            m_capacity = capacity;
-            m_array = new int[m_capacity];
+            Capacity = capacity;
+            m_array = new int[Capacity];
         }
-
-        /*public MyList(int capacity, int count) 
-        {
-            m_capacity = capacity;
-            m_count = count;
-        }*/
 
         public int this[int index]
         {
             get
             {
+                CheckIndexRange(index);
                 return m_array[index];
             }
             set
             {
+                CheckIndexRange(index);
                 m_array[index] = value;
             }
+        }
+
+        private void IncreaseCapacityIfNeed()
+        {
+            if (Count == Capacity)
+            {
+                Capacity *= 2;
+            }
+        }
+
+        private bool CheckIndexRange(int index)
+        {
+            if (index < 0 || index >= Count)
+                throw new ArgumentOutOfRangeException();
+
+            return true;
         }
 
         public void Print()
@@ -91,46 +81,36 @@ public class MyListScript : MonoBehaviour
 
         public void Add(int item)
         {
-            m_count++;
-            if (m_count <= m_capacity)
-            {
-                m_array[m_count - 1] = item;
-            }
-            else
-            {
-                throw new Exception("Invalid count");
-            }
+            Count++;
+            IncreaseCapacityIfNeed();
+            m_array[Count-1] = item;
 
         }
 
         public void Insert(int index, int item)
         {
-            m_count += 1;
+            Count += 1;
             bool flag = false;
             int _item = 0;
             int _curr;
-            if (m_count <= m_capacity)
+            if (Count > Capacity)
             {
-                for (int i = 0; i < m_array.Length; i++)
-                {
-                    if (flag)
-                    {
-                        _curr = m_array[i];
-                        m_array[i] = _item;
-                        _item = _curr;
-                    }
-                    if (index - 1 == i)
-                    {
-                        _item = m_array[i];
-                        m_array[i] = item;
-                        flag = true;
-                    }
-                }
+                IncreaseCapacityIfNeed();
             }
-            else
+            for (int i = 0; i < m_array.Length; i++)
             {
-                m_count--;
-                throw new Exception("Invalid count");
+                if (flag)
+                {
+                    _curr = m_array[i];
+                    m_array[i] = _item;
+                    _item = _curr;
+                }
+                if (index - 1 == i)
+                {
+                    _item = m_array[i];
+                    m_array[i] = item;
+                    flag = true;
+                }
             }
         }
 
@@ -143,17 +123,18 @@ public class MyListScript : MonoBehaviour
                     return i;
                 }
             }
-            throw new Exception("Wrong index"); //СѓР±СЂР°С‚СЊ СЌС‚Рѕ, Р·Р°РјРµРЅРёС‚СЊ РЅР° return
+            return -1;
         }
 
         public void Remove(int item)
         {
+            int _m_count = Count;
             bool flag = false;
-            for (int i = 0; i < m_array.Length - 1; i++) 
+            for (int i = 0; i < _m_count; i++) 
             {
                 if (m_array[i] == item)
                 {
-                    m_count--;
+                    Count--;
                     m_array[i] = m_array[i + 1];
                     flag = true;
                 }
@@ -161,18 +142,17 @@ public class MyListScript : MonoBehaviour
                 {
                     m_array[i] = m_array[i + 1];
                 }
-                if (i== m_array.Length - 1)
+                if (i== Count)
                 {
                     m_array[i] = 0;
                 }
             }
-            // Print();
         }
 
         public void RemoveAt(int index)
         {
             bool flag = true;
-            for (int i = 0;i < m_array.Length - 1; i++)
+            for (int i = 0;i < Count; i++)
             {
                 if (flag)
                 {
@@ -188,40 +168,224 @@ public class MyListScript : MonoBehaviour
 
         public bool Contains(int item)
         {
-            for (int i = 0; i < m_array.Length; i++) 
-            {
-                if (m_array[i] == item) 
-                {
-                    return true;
-                }
-            }
-            return false;
+            return IndexOf(item) >= 0;
         }
 
         public void Clear()
         {
+            System.Array.Clear(m_array, 0, m_array.Length);
+            Count = 0;
+        }
+    }
+
+    public class MyList <TItem>
+    {
+        private TItem[] m_array = new TItem[10];
+
+        public int Count { get; private set; }
+
+        public int Capacity
+        {
+            get
+            {
+                return m_array.Length;
+            }
+            set
+            {
+                if (value > m_array.Length)
+                {
+                    System.Array.Resize(ref m_array, value);
+                }
+            }
+        }
+
+        public MyList()
+        {
+
+        }
+
+        public MyList(int capacity)
+        {
+            Capacity = capacity;
+            m_array = new TItem[Capacity];
+        }
+
+        public TItem this[int index]
+        {
+            get
+            {
+                CheckIndexRange(index);
+                return m_array[index];
+            }
+            set
+            {
+                CheckIndexRange(index);
+                m_array[index] = value;
+            }
+        }
+
+        private void IncreaseCapacityIfNeed()
+        {
+            if (Count == Capacity)
+            {
+                Capacity *= 2;
+            }
+        }
+
+        private bool CheckIndexRange(int index)
+        {
+            if (index < 0 || index >= Count)
+                throw new ArgumentOutOfRangeException();
+
+            return true;
+        }
+
+        public void Print()
+        {
             for (int i = 0; i < m_array.Length; i++)
             {
-                m_array[i] = 0;
+                Debug.Log("Index = " + i + " Element = " + m_array[i]);
             }
+        }
+
+        public void Add(TItem item)
+        {
+            Count++;
+            IncreaseCapacityIfNeed();
+            m_array[Count - 1] = item;
+
+        }
+
+        public void Insert(int index, TItem item)
+        {
+            Count += 1;
+            bool flag = false;
+            TItem _item = default;
+            TItem _curr;
+            if (Count > Capacity)
+            {
+                IncreaseCapacityIfNeed();
+            }
+            for (int i = 0; i < m_array.Length; i++)
+            {
+                if (flag)
+                {
+                    _curr = m_array[i];
+                    m_array[i] = _item;
+                    _item = _curr;
+                }
+                if (index - 1 == i)
+                {
+                    _item = m_array[i];
+                    m_array[i] = item;
+                    flag = true;
+                }
+            }
+        }
+
+        /*public int IndexOf(TItem item)
+        {
+            for (int i = 0; i < m_array.Length; i++)
+            {
+                if (m_array[i] == item)
+                {
+                    return i;
+                }
+            }
+            return -1;
+        }
+
+        public void Remove(TItem item)
+        {
+            int _m_count = Count;
+            bool flag = false;
+            for (int i = 0; i < _m_count; i++)
+            {
+                if (m_array[i] == item)
+                {
+                    Count--;
+                    m_array[i] = m_array[i + 1];
+                    flag = true;
+                }
+                if (flag)
+                {
+                    m_array[i] = m_array[i + 1];
+                }
+                if (i == Count)
+                {
+                    m_array[i] = default;
+                }
+            }
+        }*/
+
+        public void RemoveAt(int index)
+        {
+            bool flag = true;
+            for (int i = 0; i < Count; i++)
+            {
+                if (flag)
+                {
+                    m_array[i] = m_array[i + 1];
+                }
+                if (i == index)
+                {
+                    m_array[i] = m_array[i + 1];
+                    flag = true;
+                }
+            }
+        }
+
+        /*public bool Contains(TItem item)
+        {
+            return IndexOf(item) >= 0;
+        }*/
+
+        public void Clear()
+        {
+            System.Array.Clear(m_array, 0, m_array.Length);
+            Count = 0;
         }
     }
 
 
     void Start()
     {
-        MyList myList = new MyList();
+        MyList<int> myList = new MyList<int>();
         myList.Add(1);
         myList.Add(5);
         myList.Insert(1, 3);
-        myList.Capacity = 4;
-        myList.Remove(3);
+        //myList.Capacity = 4;
+        //myList.Remove(3);
         myList.RemoveAt(0);
 
         for (int i = 0; i < myList.Count; ++i)
         {
             Debug.Log(myList[i]);
         }
+
+        System.Diagnostics.Stopwatch sw = System.Diagnostics.Stopwatch.StartNew();
+        MyList<MyClass> myList1 = new MyList<MyClass>();
+        for (int i = 0; i < 1000000; i++)
+        {
+            myList1.Add(new MyClass());
+        }
+        myList1.RemoveAt(5000);
+        myList1.RemoveAt(250000);
+        myList1.Insert(555555, new MyClass());
+        Debug.Log($"MyList time {sw.ElapsedMilliseconds}");
+
+        List<MyClass> standartList = new List<MyClass>();
+        for (int i = 0; i < 1000000; i++)
+        {
+            standartList.Add(new MyClass());
+        }
+
+        standartList.RemoveAt(5000);
+        standartList.RemoveAt(250000);
+        standartList.Insert(555555, new MyClass());
+        Debug.Log($"StandartList time {sw.ElapsedMilliseconds}");
+
+
 
     }
 
